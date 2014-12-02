77850d6b839ccc52a05236a6650bfc7b0b938ed0
Chris Arnold <crag47@mail.missouri.edu>
2014-12-02 13:39:01 -0600

    Throw

---
+++ Throw/src/Throw.cpp
@@ -0,0 +1,47 @@
+//============================================================================
+// Name        : Throw.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include "Puma_OP.h"
+
+using namespace std;
+
+int main() {
+	ofstream f;
+	f.open("/usr/local/pipes/gripper");
+
+
+	// Start Position
+	MOVETO_JOINTS_WO_CHECK(-15, -190, 90, 320, 90, 90);
+
+	// Sleep
+	sleep(2);
+	setSpeed(200);
+
+	// End Position
+	MOVETO_JOINTS_WO_CHECK(-90, -160, 90, 0, 90, 90);
+
+
+	// Sleep
+	usleep(715000);
+
+	// Open gripper
+	f << 'o' << endl;
+
+	sleep(1);
+	setSpeed(50);
+
+	f.close();
+
+	return 0;
+}
+

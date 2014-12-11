a9f7d8c5925bdf4aae205859da52190e020ec5e4
Dallas Fletchall <dallas.fletchall@gmail.com>
2014-12-10 20:55:34 -0600

    Update the improcessing to use multiple intensities

--- Driver.m
+++ Driver.m
@@ -3,8 +3,10 @@ close all;
 addpath('./dependencies/');
 addpath('./Throw/Debug/');
 
-% Define distance for objects that can use easy pickup
+% Definitons
 easy_distance = 100;
+areas = [ 500, 5000, 10000 ];
+tolerance = 10;
 
 % Define which puma we are using
 puma_number = '1';
@@ -17,63 +19,71 @@ r_p = importdata('dependencies/right_p_matrix.mat');
 puma_speed(20);
 puma_ready();
 puma_speed(100);
+puma_defense();
 
-while numobj > 0
-   
-    puma_defense();
-    
+for j = 1:10 % THIS WILL HAVE TO BE CHANGED IN FINAL PROJECT
+
     % Process the images
     take_pictures(puma_number);
-    [l_pic, l_n] = improcessing('left.ppm', 1, 1);
-    [r_pic, r_n] = improcessing('right.ppm', 1, 2);
-    
-    % Verify that the same number of objects exist in each file
-    if l_n ~= r_n
-        dsp('Images contain different number of objects');
-        continue;
-    end
 
-    % Pick the first easily accessible object
-    obj_found = 0;
-    for i = l_n:-1:1
-        
-        % Remove all but one object
-        l_temp_pic = (l_pic == i);
-        r_temp_pic = (r_pic == i);
-        
-        % Find the centroid in each picture
-        l_cent = regionprops(l_temp_pic, 'Centroid');
-        r_cent = regionprops(r_temp_pic, 'Centroid');
-        points(:,1) = uv2xyz(l_cent.Centroid(1,:).', l_p, r_cent.Centroid(1,:).', r_p);
-        
-        % add offset between robot frame and camera frame
-        xyz = points(:, 1).' + [-410, 140, -190];
-        
-        % Continue if object found is not easily accessible
-        if sqrt(xyz(1)^2 + xyz(2)^2) < easy_distance
-            
-            % An easy object has been found, find it's orientation
-            obj_found = 1;
-            l_orient = orientation(l_temp_pic);
-            r_orient = orientation(r_temp_pic);
-            orient = l_orient + r_orient / 2;
-            break;
-            
+    % Process image with different intensities
+    for i = .9:-.1:.1
+
+        [l_pic, l_n] = improcessing('left.ppm',i, areas,1,1);
+        [r_pic, r_n] = improcessing('right.ppm',i,areas,1,2);
+
+        % Is object detected at this intensity
+        if l_n == r_n && l_n ~= 0
+
+            % Find the first easily accessible object
+            for n = l_n:-1:1
+
+                % Remove all but one object
+                l_temp_pic = (l_pic == n);
+                r_temp_pic = (r_pic == n);
+
+                % Find the centroid in each picture
+                t_cent = regionprops(l_temp_pic, 'Centroid');
+                l_cent = t_cent.Centroid(1,:).';
+                t_cent = regionprops(r_temp_pic, 'Centroid');
+                r_cent = t_cent.Centroid(1,:).';
+                xyz = uv2xyz(l_cent, l_p, r_cent, r_p).';
+                
+                % Check that we are looking at the same object
+                if abs(xyz(3)) > tolerance
+                    %%%%% This should remove the chance that we are looking
+                    %%%%% at a different object in each camera
+                   continue; 
+                end
+                
+                % Set offset for robot coordinate frame
+                xyz(1) = xyz(1) - 410;
+                xyz(2) = xyz(2) + 140;
+                xyz(3) = -190;
+
+                % Do nothing if object found is not easily accessible
+                if sqrt(xyz(1)^2 + xyz(2)^2) < easy_distance
+
+                    % An easy object has been found, find it's orientation
+                    l_orient = orientation(l_temp_pic);
+                    r_orient = orientation(r_temp_pic);
+                    orient = l_orient + r_orient / 2;
+
+                    % Pick-up and throw the object
+                    %pickup_object(xyz, orient);
+                    %throw_object();
+                    %puma_defense();
+                    
+                end
+            end
         end
         
+        % When i == .1, check for adjacent objects
+        % process image with different area
+        % drill()
+        % puma_defense()
+        
     end
-    
-    % Pick up an object or process the image for objects touching
-    if obj_found == 1
-        pickup_object(xyz, orient);
-        throw_object();
-    else
-        % find_duplicate_objs();
-        % Get the centroid of the double object
-        % drill(); the objects to a new position
-    end
-    
-    
 end
 
 % Place puma in nest position
--- improcessing.m
+++ improcessing.m
@@ -1,27 +1,38 @@
-function [image, n] = improcessing( image, display, figure_num )
-% Returns the image with objects already categorized into n objects
+function [image, n] = improcessing(image, intensity, areas, display, f_num)
+% Less selective image processing to determine if objects are on edge of
+% box
 
-    if display == 0
-        % This should run faster since memory allocation is unnecessary
-        image = imread(image); % Read in image
-        image = im2bw(image,.9); % Convert to black and white
-        image = bwareaopen(image, 10000, 8); % Remove small inconsistencies
-        % Remove objects too small
-        image = bwareaopen(imcomplement(image), 5000, 8);
-        % Remove objects too big
-        image = image - bwareaopen(image, 10000, 8); 
-        % Remove all but the closest object
-        [image, n] = bwlabel( image, 8); % Same procedure, but saves each individual image to display
-        
-    else
-        figure(figure_num);
-        i1 = imread(image); subplot(3,2,1); subimage(i1);
-        i2 = im2bw(i1,.9); subplot(3,2,2); subimage(i2);
-        i3 = bwareaopen(i2, 10000, 8);subplot(3,2,3); subimage(i3);
-        i4 = bwareaopen(imcomplement(i3), 5000, 8);subplot(3,2,4); subimage(i4);
-        i5 = i4 - bwareaopen(i4, 10000, 8);subplot(3,2,5); subimage(i5);
-        [i6, n] = bwlabel( i5, 8);
-        image = i6;
+    % Read in the image
+    image = imread(image);
+    if display == 1
+        figure(f_num); subplot(3,2,1); subimage(image);
     end
-   
+    
+    % Convert the image to black and white
+    image = im2bw(image, intensity);
+    if display == 1
+        subplot(3,2,2); subimage(image);
+    end
+    
+    % Remove small inconsistencies
+    image = bwareaopen(image, areas(1), 8);
+    if display == 1
+        subplot(3,2,3); subimage(image);
+    end
+    
+    % Remove areas too small to be objects
+    image = bwareaopen(imcomplement(image), areas(2), 8);
+    if display == 1
+        subplot(3,2,4); subimage(image);
+    end
+    
+    % Remove areas too big to be objects
+    image = image - bwareaopen(image, areas(3), 8);
+    if display == 1
+        subplot(3,2,5); subimage(image);
+    end
+    
+    [image, n] = bwlabel(image, 8);
+
 end
+

084ad0c66e39a32c99bdab42a67a91fc2b75fa4f
Dallas Fletchall <dallas.fletchall@gmail.com>
2014-12-12 13:31:58 -0600

    Modify pickup_object with approach

--- pickup_object.m
+++ pickup_object.m
@@ -14,6 +14,9 @@ o = 0;
 a = 90;
 t = orientation;
 
+x_pad = x;
+y_pad = y;
+
 theta = atan2d(y,x);
 radius = sqrt(x^2 + y^2);
 
@@ -28,6 +31,24 @@ if radius > robot_radius
     a = asind((d_obj) / (d_gripper_center)) + 90;
     robot_z = robot_z - (d_tool - sqrt(d_tool^2 - d_obj^2));
     
+    % choose tool angle based on quadrant
+    % quad I
+    if y < 222
+        t = t + 68;
+        x_pad = robot_x + 40;
+        y_pad = robot_y;
+    %quad II
+    elseif x < -340
+        t = t + 68;
+        x_pad = robot_x + 40;
+        y_pad = robot_y - 40;
+    %quad III and IV
+    elseif x < -266
+        t = t + 48;
+        x_pad = robot_x;
+        y_pad = robot_y - 20;
+    end
+    
 % else use coordinates of centroid
 else
     robot_x = x;
@@ -40,7 +61,8 @@ XYZ_OAT = [robot_x, robot_y, robot_z, o, a, t];
     % set XYZOAT
     puma_speed(20);
     puma_moveto_xyzoat(robot_x, robot_y, -100, o, a, t);
-    puma_moveto_xyzoat(robot_x+40, robot_y-40, robot_z+40, o, a, t);
+    gripper('o');
+    puma_moveto_xyzoat(x_pad, y_pad, robot_z + 40, o, a, t);
     
     % lower gripper
     puma_speed(20);
